161edb144dea681a1a64a960533c56658735e0a4
Davvii1 <david2002rg@hotmail.com>
2023-04-23 16:00:23 -0600

    proxy fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://neuro-sketch-backend.vercel.app"
+      "/api": {
+        target: 'https://neuro-sketch-backend.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   },
   plugins: [react()],
