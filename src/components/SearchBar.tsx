76cb0b9bb12da640427495e1682f58e3505f613f
Davvii1 <david2002rg@hotmail.com>
2023-05-08 17:42:47 -0600

    navbar fix mobile

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,7 +2,8 @@ import '../styles/components/SearchBar.css'
 import Icon from '../assets/svgs/Search.svg'
 import Button from './Button'
 import { useSearchParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { MessageContext } from '../context/MessageContext'
 
 interface Props {
     width?: string,
@@ -11,10 +12,19 @@ interface Props {
 
 const SearchBar = (props: Props) => {
     const [searchParams] = useSearchParams();
+    const { setMessage } = useContext(MessageContext)
     const [searchValue, setSearchValue] = useState(searchParams.get('search'));
 
     const setValues = (value: string) => {
         setSearchValue(value);
+    }
+
+    const GoSearch = () => {
+        if (searchValue != null) {
+            window.location.href = `/search?&search=${searchValue}`;
+        } else {
+            setMessage("Set prompt value");
+        }
     }
 
     return (
@@ -23,7 +33,7 @@ const SearchBar = (props: Props) => {
                 <img src={Icon} id='search' alt="Search" />
                 <input id='bar' type="text" value={searchValue || ''} onChange={(e) => setValues(e.target.value)} autoComplete='off' style={{ border: props.border ? "1px solid black" : "" }} />
             </div>
-            <Button text='Go' onClick={() => window.location.href = `/search?&search=${searchValue}`} height='3.125rem' fontSize='1rem' />
+            <Button text='Go' onClick={() => GoSearch()} height='3.125rem' fontSize='1rem' />
         </div>
     )
 }
